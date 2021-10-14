876f0cfed72986e82cd2b2df834ea41b5f779820
Nazmus Sakib <sakibkhancse09@gmail.com>
2021-10-15 04:30:06 +0600

    Hash Link working done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import { HashLink } from 'react-router-hash-link';
+
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -12,11 +14,11 @@ const Header = () => {
                     <Navbar.Brand href="#home">Gineus Car Mechanics</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
-                        <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#experts">Experts</Nav.Link>
                         {user?.email ? <Button onClick={logOut} varient="light">Log Out</Button>
-                            : <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+                            : <Nav.Link as={HashLink} to="/login">Login</Nav.Link>}
                         <Navbar.Text>
                             Signed in as: <a href="#login">{user?.displayName}</a>
                         </Navbar.Text>
